2f9fdf57e3bf64d2b2d9ade7f920bbaac4dd9bc6
javavinoth <vinoth.sakthivel95@gmail.com>
2018-10-20 13:52:58 +0530

    First Commit

---
+++ email-service-v1/src/main/java/com/apibucket/emailservice/EmailServiceV1Application.java
@@ -0,0 +1,12 @@
+package com.apibucket.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServiceV1Application.class, args);
+	}
+}
